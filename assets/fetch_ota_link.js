c8316560f577bafa08359a4ae81bca8cec2e2ffd
nhansp <nhan.094.vn@gmail.com>
2024-09-08 11:18:56 +0700

    web: add commit message as header
    Co-authored-by: Yuuki2k6 <phuocduy252@gmail.com>

--- assets/fetch_ota_link.js
+++ assets/fetch_ota_link.js
@@ -1,3 +1,11 @@
+// fetch latest commit from OTA repo
+fetch('https://api.github.com/repos/tvyiutnhisokewt/OTA_Evolution-X/commits?per_page=1')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('latest-commit').innerHTML = "latest push is: " + res[0].commit.message;
+  })
+
+// fetch download links
 fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/builds/bluejay.json')
   .then(res => res.json())
   .then(res => {
@@ -8,3 +16,4 @@ fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/bu
   .then(res => {
     document.getElementById('coral').href = res.response[0].download
   })
+  

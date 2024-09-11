11a546d2d023eab305380fdf77baa1d688db7994
nhansp <nhan.094.vn@gmail.com>
2024-09-12 05:05:28 +0700

    fuck im too sleepy to describe changes

--- assets/fetch_ota_link.js
+++ assets/fetch_ota_link.js
@@ -1,19 +1,35 @@
 // fetch latest commit from OTA repo
-fetch('https://api.github.com/repos/tvyiutnhisokewt/OTA_Evolution-X/commits?per_page=1')
+fetch('https://api.github.com/repos/tvyiutnhisokewt/OTA_Evolution-X/commits?path=builds/coral.json&per_page=1')
   .then(res => res.json())
   .then(res => {
-    document.getElementById('latest-commit').innerHTML = "latest push is: " + res[0].commit.message;
+    document.getElementById('commit-coral').innerHTML = res[0].commit.message;
+  })
+fetch('https://api.github.com/repos/tvyiutnhisokewt/OTA_Evolution-X/commits?path=builds/bluejay.json&per_page=1')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('commit-bluejay').innerHTML = res[0].commit.message;
   })
 
 // fetch download links
 fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/builds/bluejay.json')
   .then(res => res.json())
   .then(res => {
-    document.getElementById('bluejay').href = res.response[0].download
+    document.getElementById('dl-bluejay').href = res.response[0].download
   })
 fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/builds/coral.json')
   .then(res => res.json())
   .then(res => {
-    document.getElementById('coral').href = res.response[0].download
+    document.getElementById('dl-coral').href = res.response[0].download
   })
-  
+
+// fetch filenames
+fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/builds/bluejay.json')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('filename-bluejay').innerHTML = res.response[0].filename
+  })
+fetch('https://raw.githubusercontent.com/tvyiutnhisokewt/OTA_Evolution-X/main/builds/coral.json')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('filename-coral').innerHTML = res.response[0].filename
+  })
